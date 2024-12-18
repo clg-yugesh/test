25566ab3a85602c429c5c4ed12414908a3e650aa
Yugesh <242237@slcs.edu.in>
2024-12-18 11:58:41 +0530

    uh..

---
+++ Python/Excercise/tuples/16.py
@@ -0,0 +1,8 @@
+tup1 = (1,2,3)
+tup2 = (3,4,5)
+
+combain = list(tup1) + list(tup2)
+
+tup3 = tuple(combain)
+
+print(tup3)
---
+++ Python/Excercise/tuples/17.py
@@ -0,0 +1,4 @@
+tup = (10, 20, 30, 40, 50)
+
+print("First element : ",tup[0])
+print("Second element : ",tup[-1])
